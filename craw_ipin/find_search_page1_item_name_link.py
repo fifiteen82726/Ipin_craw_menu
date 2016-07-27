2bba5240ddf36b80ed424f6b9d455d52c23b7353
fifiteen82726 <fifiteen82726@gmail.com>
2016-07-28 00:10:59 +0800

    finish craw, 海鮮都沒菜單？可能有bug

--- craw_ipin/find_search_page1_item_name_link.py
+++ craw_ipin/find_search_page1_item_name_link.py
@@ -32,18 +32,28 @@ def get_menu(restaurant_url):
 # Find kind list to restaurant page
 res = requests.get('http://www.ipeen.com.tw/taiwan/channel/F')
 soup = BeautifulSoup(res.text)
+
+# 0 = 海鮮餐廳
 restaurant = soup.select('.detail li a')[0].get('href')
 food_kind_url = 'http://www.ipeen.com.tw'+restaurant
 
-res = requests.get(food_kind_url)
-soup = BeautifulSoup(res.text)
-# print food_kind_url
-# find every restaurant name and url
-for restaurant in soup.select('.serShop h3'):
-	if (restaurant.text.strip()[0].isdigit() == True):
-		print restaurant.text.strip().replace("\t", "").replace("\n","").replace(" ","")
-		print restaurant.select('a')[0].get('href')
-		get_menu(restaurant.select('a')[0].get('href'))
+page = 1
+
+for i in range(1,27):
+	if i == 1:
+		res = requests.get(food_kind_url)
+	else:
+		res = requests.get(food_kind_url+'/?p='+str(i))
+		print food_kind_url+'/?p='+str(i)
+	soup = BeautifulSoup(res.text)
+	# print food_kind_url
+	# find every restaurant name and url
+	for restaurant in soup.select('.serShop h3'):
+		if (restaurant.text.strip()[0].isdigit() == True):
+			print restaurant.text.strip().replace("\t", "").replace("\n","").replace(" ","")
+			print restaurant.select('a')[0].get('href')
+			#抓下menu，如果有的話
+			get_menu(restaurant.select('a')[0].get('href'))
         # requests.get('http://www.ipeen.com.tw'+restaurant.select('a')[0].get('href'))
 
 ## have not confirm
